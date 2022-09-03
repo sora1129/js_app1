ea992e8e4c35a9fee947aa6667dbdab28e2d275a
sora1129 <yuka.izumisawa@sweeep.ai>
2022-09-03 19:57:27 +0900

    js:btn event

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    btn.textContent = 'hit!';
+    const results = ['大吉', '中吉', '凶', '末吉'];
+    const n = Math.floor(Math.random() * results.length);
+    btn.textContent = results[n];
   });
 }
